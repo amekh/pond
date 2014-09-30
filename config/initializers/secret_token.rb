ed4919007e48cd5016cbac6df2c74fd0c3533643
kouki <kouki@kouki-no-MacBook-Air.local>
2014-09-30 23:42:53 +0900

    プロジェクト作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pond::Application.config.secret_key_base = '5d49adbe1a1397a683eb75abc2f8ccba4c0c19324d97d6f59c7a588e4378ec36b8da04a105ac81b7b0545b50c64e44c93deabb7f57884f9616264da11ffaf398'
