69cef21d60547e6b87ea0b05636bd9f1bbe852d6
kouki <higashikawa.kouki@headwaters.co.jp>
2014-10-03 23:31:14 +0900

    ログイン認証追加
    - ログインページ作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ Pond::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.sass.preferred_syntax = :sass
+  config.sass.line_comments = false
+  config.sass.cache = false
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003010710) do
+ActiveRecord::Schema.define(version: 20141003030448) do
 
   create_table "companies", force: true do |t|
     t.string   "name",       null: false
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20141003010710) do
   end
 
   create_table "sections", force: true do |t|
-    t.string   "name"
+    t.string   "name",           null: false
     t.integer  "leader_user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -75,11 +75,22 @@ ActiveRecord::Schema.define(version: 20141003010710) do
   create_table "users", force: true do |t|
     t.integer  "devision_id"
     t.integer  "section_id"
-    t.string   "login_name",     null: false
-    t.string   "login_password", null: false
-    t.string   "name",           null: false
+    t.string   "name",                                null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
