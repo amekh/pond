5119f9a612b96d0fc29361f9cd6772d9d857e33c
kouki <higashikawa.kouki@headwaters.co.jp>
2014-10-04 00:23:50 +0900

    Indexへルーティング追加、Gravater画像を利用できるメソッド定義
    - home#indexへルーティング定義追加 ( root:to )
    - Gravater画像を利用できるHelperメソッドを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# coding: utf-8
 module ApplicationHelper
+
+  #
+  # Gravatarに登録してある画像URLに変換
+  #
+  # @param [String] email_address emailアドレス
+  # @return [String] Gravatarの画像URL
+  #
+  def avatar_url( email_address )
+    default_url = "#{root_url}assets/default.png"
+    gravatar_id = Digest::MD5.hexdigest(email_address.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=40&d=#{CGI.escape(default_url)}"
+  end
+  
 end
