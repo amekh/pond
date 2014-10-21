9ea273e083c4acf0ce57782a8d82a6a768f3a20b
kouki <higashikawa.kouki@headwaters.co.jp>
2014-10-21 23:11:11 +0900

    ログインページはレイアウトを利用しないようにしました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,27 @@
+# coding: utf-8
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
+  layout :layout_by_resource
+  
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  # コントローラによってレイアウトを振り分ける
+  # @return [String] レイアウト名
+  # @return [boolean] レイアウトがない場合 => false
+  def layout_by_resource
+    if devise_controller?
+      false
+    else
+      "application"
+    end
   end
     
 end
