121e562e8e0ee3f4e3bf9e39576f0484d9d81d9b
kouki <higashikawa.kouki@headwaters.co.jp>
2014-10-26 00:21:50 +0900

    missionモデルに、companyとmission_typeを関連づけました。

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
 class Company < ActiveRecord::Base
+
+  attr_accessor :id, :name
+  
+  has_many :mission
+
 end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,2 +1,11 @@
 class Mission < ActiveRecord::Base
+  attr_accessor :id, :company_id, :mission_type_id, :title,
+                :contents, :nes_work_time, :unnes_work_time_min,
+                :unnes_work_time_max
+
+  has_many :unit_mission
+  belongs_to :company, :mission_type
+
+  
+
 end
